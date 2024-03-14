25861b84e59080db9de593c26bd0cc0219af7be4
Alex McKean <alexrmckean@gmail.com>
2024-03-14 19:58:01 +0000

    Tailwind nav/ Budget List

--- ghi/src/Budget/BudgetList.jsx
+++ ghi/src/Budget/BudgetList.jsx
@@ -18,33 +18,88 @@ function DeleteButton({ budgetId }) {
 
 function Budget() {
     const { data: budgets = [] } = useBudgetsQuery();
+    function DeleteButton({ budgetId }) {
+        const [deleteBudget] = useDeleteBudgetMutation();
+
+        const handleDelete = async () => {
+            try {
+                await deleteBudget(budgetId);
+
+            } catch (error) {
+                console.error('Error deleting budget:', error);
+            }
+        };
+
+        return <button onClick={handleDelete}>Delete</button>;
+    }
 
     return (
-        <div>
-            <h1>My Budgets</h1>
-            <ul>
-                {budgets.map((budget) => (
-                    <div key={budget.id}>
-                        <p>
-                            Description: {budget.description}
-                            <div>Amount: {budget.amount}</div>
-                            <div>Date: {budget.date}</div>
-                            <div>Payment Method: {budget.payment_method}</div>
-                            <div>
-                                <DeleteButton budgetId={budget.id} />
-                                <Link to={`/api/budgets/edit/${budget.id}`}>
-                                    <button>Edit</button>
-                                </Link>
-                            </div>
-                        </p>
-                    </div>
-                ))}
-            </ul>
-            <Link to="/api/budgets/create/">
-                <button type="button">Create a budget</button>
-            </Link>
-        </div>
+        <>
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg pt-20">
+                <h1 class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white pb-8 pt-4 pl-4">My Budget</h1>
+            </div>
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Date
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Description
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Payment Method
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Amount
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Edit
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Delete
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {budgets.map((budget) => (
+                            <tr key={budget.id} class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                                <td class="px-6 py-4">
+                                    {budget.date}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {budget.description}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {budget.payment_method}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {budget.amount}
+                                </td>
+                                <td class="px-6 py-4">
+                                    <Link to={`/api/budgets/edit/${budget.id}`}>
+                                        <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Edit</button>
+                                    </Link>
+                                </td>
+                                <td class="px-6 py-4">
+                                    <button class="text-white bg-red-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                        < DeleteButton budgetId={budget.id} />
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg pt-4 pb-4 pl-4">
+                    <Link to="/api/budgets/create/">
+                    <button type="button" class="text-white bg-green-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Budget Item</button>
+                    </Link>
+                </div>
+            </div>
+        </>
     );
+
 }
 
 export default Budget;
--- ghi/src/Components/Nav.jsx
+++ ghi/src/Components/Nav.jsx
@@ -1,33 +1,44 @@
-import { NavLink } from "react-router-dom";
 import LogoutButton from './LogoutButton';
-
+import Logo from '../images/Logo.png';
+// import './index.css'
+import { NavLink } from 'react-router-dom';
 function Nav() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-success">
-            <div className="container-fluid">
-                <NavLink className="navbar-brand" to="/">Travel Tracers</NavLink>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="api/signup">Sign Up</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="api/login">Sign In</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="api/budgets">Budget</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="api/packing_list">Packing List</NavLink>
-                     </li>
-                    {LogoutButton}
-                        <li className="nav-item">
-                            <LogoutButton />
-                    </li>
-                </ul>
-                </div>
-            </div>
-        </nav>
+    <nav class="bg-gray-800 dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <img src={Logo} class="h-8" alt="Logo"/>
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white text-white">Travel Tracers</span>
+    </a>
+    <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+        <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+            </svg>
+        </button>
+    </div>
+    <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+        <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-gray-800 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <li>
+            <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-white md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+        </li>
+        <li>
+            <a href="/api/itinerary" class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Itinerary</a>
+        </li>
+        <li>
+            <a href="/api/budgets" class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Budget</a>
+        </li>
+        <li>
+            <a href="/api/packing_list" class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Packing List</a>
+        </li>
+         <li>
+            <a href="/api/login" class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Login</a>
+        </li>
+        </ul>
+    </div>
+    </div>
+    </nav>
     );
 }
 export default Nav;
